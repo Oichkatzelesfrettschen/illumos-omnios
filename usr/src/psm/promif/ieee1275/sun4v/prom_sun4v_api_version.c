a0177ae0cfcc1e345b5933bf9c51eadfa42c9c29
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-06-08 19:34:55 -0700

    Add Doxygen and Sphinx integration

--- usr/src/psm/promif/ieee1275/sun4v/prom_sun4v_api_version.c
+++ usr/src/psm/promif/ieee1275/sun4v/prom_sun4v_api_version.c
@@ -23,68 +23,74 @@
  * Use is subject to license terms.
  */
 
-#pragma ident	"%Z%%M%	%I%	%E% SMI"
+#pragma ident "%Z%%M%	%I%	%E% SMI"
 
+#include <sys/hypervisor_api.h>
 #include <sys/promif.h>
 #include <sys/promimpl.h>
-#include <sys/hypervisor_api.h>
 
-/*
- * This interface allows the client to negotiate a major/minor
- * version number for the specified sun4v API group.
+/**
+ * Negotiates a sun4v API version with the firmware.
+ *
+ * @param api_group Identifier for the API group to negotiate.
+ * @param major     Desired major version number.
+ * @param minor     Desired minor version number.
+ * @param supported_minor  Updated with the minor version supported by
+ *                         the firmware.
+ * @return Hypervisor error code from the firmware call.
  */
-uint64_t
-prom_set_sun4v_api_version(uint64_t api_group, uint64_t major, uint64_t minor,
-	uint64_t *supported_minor)
-{
-	cell_t ci[8];
+uint64_t prom_set_sun4v_api_version(uint64_t api_group, uint64_t major,
+                                    uint64_t minor, uint64_t *supported_minor) {
+  cell_t ci[8];
 
-	if (prom_test("SUNW,set-sun4v-api-version") != 0)
-		return (H_EBADTRAP);
+  if (prom_test("SUNW,set-sun4v-api-version") != 0)
+    return (H_EBADTRAP);
 
-	ci[0] = p1275_ptr2cell("SUNW,set-sun4v-api-version");	/* Service */
-	ci[1] = (cell_t)3;			/* #argument cells */
-	ci[2] = (cell_t)2;			/* #result cells */
-	ci[3] = (cell_t)api_group;		/* Arg1: api_group */
-	ci[4] = (cell_t)major;			/* Arg2: major */
-	ci[5] = (cell_t)minor;			/* Arg3: minor */
-	ci[6] = (cell_t)-1;			/* Res1: status */
-	ci[7] = (cell_t)-1;			/* Res2: Supported minor */
+  ci[0] = p1275_ptr2cell("SUNW,set-sun4v-api-version"); /* Service */
+  ci[1] = (cell_t)3;                                    /* #argument cells */
+  ci[2] = (cell_t)2;                                    /* #result cells */
+  ci[3] = (cell_t)api_group;                            /* Arg1: api_group */
+  ci[4] = (cell_t)major;                                /* Arg2: major */
+  ci[5] = (cell_t)minor;                                /* Arg3: minor */
+  ci[6] = (cell_t)-1;                                   /* Res1: status */
+  ci[7] = (cell_t)-1; /* Res2: Supported minor */
 
-	promif_preprom();
-	(void) p1275_cif_handler(&ci);
-	promif_postprom();
+  promif_preprom();
+  (void)p1275_cif_handler(&ci);
+  promif_postprom();
 
-	*supported_minor = (uint64_t)(ci[7]);
-	return ((uint64_t)(ci[6]));
+  *supported_minor = (uint64_t)(ci[7]);
+  return ((uint64_t)(ci[6]));
 }
 
-/*
- * This interface allows the client to get the currently negotiated
- * major/minor version number associated with the specified sun4v
- * API group.
+/**
+ * Retrieves the negotiated sun4v API version for the given group.
+ *
+ * @param api_group Identifier for the API group to query.
+ * @param major     Updated with the negotiated major version.
+ * @param minor     Updated with the negotiated minor version.
+ * @return Hypervisor error code from the firmware call.
  */
-uint64_t
-prom_get_sun4v_api_version(uint64_t api_group, uint64_t *major, uint64_t *minor)
-{
-	cell_t ci[7];
+uint64_t prom_get_sun4v_api_version(uint64_t api_group, uint64_t *major,
+                                    uint64_t *minor) {
+  cell_t ci[7];
 
-	if (prom_test("SUNW,get-sun4v-api-version") != 0)
-		return (H_EBADTRAP);
+  if (prom_test("SUNW,get-sun4v-api-version") != 0)
+    return (H_EBADTRAP);
 
-	ci[0] = p1275_ptr2cell("SUNW,get-sun4v-api-version");	/* Service */
-	ci[1] = (cell_t)1;			/* #argument cells */
-	ci[2] = (cell_t)3;			/* #result cells */
-	ci[3] = (cell_t)api_group;		/* Arg1: api_group */
-	ci[4] = (cell_t)-1;			/* Res1: status */
-	ci[5] = (cell_t)-1;			/* Res2: major number */
-	ci[6] = (cell_t)-1;			/* Res3: minor number */
+  ci[0] = p1275_ptr2cell("SUNW,get-sun4v-api-version"); /* Service */
+  ci[1] = (cell_t)1;                                    /* #argument cells */
+  ci[2] = (cell_t)3;                                    /* #result cells */
+  ci[3] = (cell_t)api_group;                            /* Arg1: api_group */
+  ci[4] = (cell_t)-1;                                   /* Res1: status */
+  ci[5] = (cell_t)-1;                                   /* Res2: major number */
+  ci[6] = (cell_t)-1;                                   /* Res3: minor number */
 
-	promif_preprom();
-	(void) p1275_cif_handler(&ci);
-	promif_postprom();
+  promif_preprom();
+  (void)p1275_cif_handler(&ci);
+  promif_postprom();
 
-	*major = (uint64_t)(ci[5]);
-	*minor = (uint64_t)(ci[6]);
-	return ((uint64_t)(ci[4]));
+  *major = (uint64_t)(ci[5]);
+  *minor = (uint64_t)(ci[6]);
+  return ((uint64_t)(ci[4]));
 }
